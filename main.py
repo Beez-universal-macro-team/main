bb51e34b521200e74a4452a1ee0ca230f960742f
Beez131Github <olivercollins123@hotmail.com>
2024-10-30 19:09:45 +1300

    fixed f2 hotkey not working while program is running (threading)

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 from pynput.keyboard import Listener, Key
 import gui
+import threading
+import time
+import keyboard
 
 running = False
+
+def check_f2():
+    while True:
+        if keyboard.is_pressed('f2'):
+            ui.window.quit()
+            break
+        time.sleep(0.2)
 
 def on_release(key):
     global running
@@ -9,19 +19,16 @@ def on_release(key):
     if key == Key.f1:
         if not running:
             running = True
-
             ui.startMacro(main=True)
 
-    elif key == Key.f2:
-        listener.stop()
-
-        ui.window.quit()
+# Start F2 checker thread
+f2_thread = threading.Thread(target=check_f2, daemon=True)
+f2_thread.start()
 
 listener = Listener(on_release=on_release)
 listener.start()
 
 ui = gui.GUI()
-
 ui.initWindow()
 ui.saveSettings()
 
