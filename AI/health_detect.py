157b9bff0d97676ef7fe2bb8720393530ceedba4
Beez131Github <olivercollins123@hotmail.com>
2024-10-30 17:11:54 +1300

    Update health_detect.py

--- AI/health_detect.py
+++ AI/health_detect.py
@@ -1,42 +1,51 @@
-import mss
-from PIL import Image
 import numpy as np
 from ultralytics import YOLO
 import os
 import sys
+import time
+import keyboard
+from functions import screenshot
+
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
-from functions import screenshot_health_area, screenshot_area, screenshot  # Add both functions
+
 # Load the YOLOv8 model
 model_path = os.path.join(os.path.dirname(__file__), 'health.pt')
 model = YOLO(model_path)
 
+
 # Convert screenshot to YOLO input format and run inference
 def detect_health_in_screenshot():
-    # Take screenshot
-    img = screenshot()
 
+    img = screenshot()
     # Convert to numpy array for YOLO input
     img = np.array(img)
 
-    # Run inference with specified confidence threshold
-    results = model(img)  # Adjust threshold as needed
+    img = img[..., ::-1]
 
-    # Define class names based on YAML
-    class_names = ['alive', 'dead']
+    # Run inference
+    results = model(img, conf=0.65)
 
-    # Iterate over detections and check if 'dead' is found
+    class_names = ['dead', 'alive']
+
+    poses = []
+
     for result in results[0].boxes:
         class_id = int(result.cls)  # Get class ID
-        confidence = result.conf  # Get confidence for the detection
         class_name = class_names[class_id]  # Map to class name
-        
+
+        x_min, y_min, x_max, y_max = result.xyxy[0].cpu().numpy().tolist()  # Assuming xyxy format is [x_min, y_min, x_max, y_max]
+
+        # Calculate center
+        pos = [(x_min + x_max) / 2, (y_min + y_max) / 2]
+
+        poses.append(pos)
+
         if class_name == 'dead':
-            print(f"Detected 'dead' with confidence {confidence}")
-            return True
+            print(f"Detected: alive, at pos {pos}")
 
-    print("No 'dead' detected")
-    return False
+        elif class_name == 'alive':
+            print(f"Detected: dead, at pos {pos}")
 
+            return False
 
-    print("No 'dead' detected")
-    return False
+    return True
