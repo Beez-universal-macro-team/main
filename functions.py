248f2b16f693103ed9095575343ada86f180ac21
Beez131Github <olivercollins123@hotmail.com>
2024-11-02 12:56:56 +1300

    Update functions.py

--- functions.py
+++ functions.py
@@ -297,7 +297,7 @@ def MoveUntilHive():
         except pyautogui.ImageNotFoundException:
             if attempt < 5:
                 keyboard.press("a")
-                Waitspeed(8)
+                Waitspeed(7)
                 keyboard.release("a")
 
             else:
