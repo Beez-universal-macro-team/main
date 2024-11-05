e25c5ef2630a0dc318a0c19cdc938db3ba18c562
pirosow <115435792+pirosow@users.noreply.github.com>
2024-11-05 07:33:34 -0500

    Update vic_detect.py

--- AI/vic_detect.py
+++ AI/vic_detect.py
@@ -17,7 +17,7 @@ except:
 if beesmas_enabled:
     model_path = os.path.join(os.path.dirname(__file__), 'vic_beesmas.pt')
 else:
-    model_path = os.path.join(os.path.dirname(__file__), 'vic_plain.pt')
+    model_path = os.path.join(os.path.dirname(__file__), 'vic_plain2.pt')
 
 model = YOLO(model_path)
 
