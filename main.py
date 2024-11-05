943a0cd7917646288a16ecaa5ea9c4d9e376d3e5
Beez131Github <olivercollins123@hotmail.com>
2024-11-05 14:25:28 +1300

    added f3, altered f2 and fixed f3/f2 keys

--- main.py
+++ main.py
@@ -1,44 +1,78 @@
-from pynput.keyboard import Listener, Key
+from pynput.keyboard import Listener, Key, Controller as keyboardController
 import gui
 import threading
 import time
 import os
+import ctypes
 
+keyboard = keyboardController()
 running = False
+macro_thread = None
 
-def check_f2():
-    listener = Listener(on_press=lambda key: key == Key.f2 and os._exit(0))
-    listener.start()
-    listener.join()
+def monitor_f2():
+    def on_press(key):
+        if key == Key.f2:
+            global macro_thread, running
+            if macro_thread and macro_thread.is_alive():
+                ctypes.pythonapi.PyThreadState_SetAsyncExc(macro_thread.ident, ctypes.py_object(SystemExit))
+                running = False
+            
+            try:
+                keyboard.release("w")
+                keyboard.release("d")
+                keyboard.release("a")
+                keyboard.release("s")
+            except:
+                pass
+
+            ui.saveSettings()
+    
+    with Listener(on_press=on_press) as f2_listener:
+        f2_listener.join()
+
+def monitor_f3():
+    def on_press(key):
+        if key == Key.f3:
+            global macro_thread, running
+            if macro_thread and macro_thread.is_alive():
+                ctypes.pythonapi.PyThreadState_SetAsyncExc(macro_thread.ident, ctypes.py_object(SystemExit))
+                running = False
+            
+            try:
+                keyboard.release("w")
+                keyboard.release("d")
+                keyboard.release("a")
+                keyboard.release("s")
+            except:
+                pass
+            os._exit(0)
+    
+    with Listener(on_press=on_press) as f3_listener:
+        f3_listener.join()
 
 def on_release(key):
-    global running
+    global running, macro_thread
     if key == Key.f1:
         if not running:
             running = True
-            ui.startMacro(main=True)
-    elif key == Key.f2:
-        ui.saveSettings()
-        os._exit(0)
+            macro_thread = threading.Thread(target=ui.startMacro, args=(True,))
+            macro_thread.start()
 
-# Start F2 checker thread with dedicated listener
-f2_thread = threading.Thread(target=check_f2, daemon=True)
+# Start dedicated monitor threads
+f2_thread = threading.Thread(target=monitor_f2, daemon=True)
 f2_thread.start()
 
-# Main listener for other keys
+f3_thread = threading.Thread(target=monitor_f3, daemon=True)
+f3_thread.start()
+
+# Main listener for F1
 listener = Listener(on_release=on_release)
+listener.daemon = True
 listener.start()
 
 ui = gui.GUI()
 ui.initWindow()
 ui.saveSettings()
-
-ui.window.mainloop()
-
-
-ui.window.mainloop()
-
-
 
 ui.window.mainloop()
 
