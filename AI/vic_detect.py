7cb1289ca2a432260d8a25388c840c1eead6cc5c
pirosow <115435792+pirosow@users.noreply.github.com>
2024-11-05 19:14:27 -0500

    New vic AI model

--- AI/vic_detect.py
+++ AI/vic_detect.py
@@ -5,7 +5,7 @@ from ultralytics import YOLO
 import os
 import sys
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
-from functions import screenshot, sendScreenshot, readFile
+from functions import *
 
 # Check beesmas toggle and load appropriate model
 try:
@@ -17,43 +17,36 @@ except:
 if beesmas_enabled:
     model_path = os.path.join(os.path.dirname(__file__), 'vic_beesmas.pt')
 else:
-    model_path = os.path.join(os.path.dirname(__file__), 'vic_plain.pt')
+    model_path = os.path.join(os.path.dirname(__file__), 'vic_plain3.pt')
 
 model = YOLO(model_path)
 
 # Convert screenshot to YOLO input format and run inference
-def detect_vic_in_screenshot():
-    # Take screenshot
-    sendScreenshot("")
-    img = screenshot()
-
+def detectVicBee(img):
     # Convert to numpy array for YOLO input
     img = np.array(img)
 
     img = img[..., ::-1]
 
     # Run inference
-    results = model(img, conf = 0.25)
+    results = model(img, conf=0.3)
 
-    # Iterate over detections and check if 'vic' or 'vic_gifted' is found
-    found_vic = False
-    found_vic_gifted = False
-    class_names = ['vic', 'vic_gifted']
+    class_names = ['vic', 'vic_gifted', 'vic_under']
+
+    poses = []
 
     for result in results[0].boxes:
         class_id = int(result.cls)  # Get class ID
         class_name = class_names[class_id]  # Map to class name
-        
-        if class_name == 'vic':
-            print("Detected: vic")
-            found_vic = True
-        elif class_name == 'vic_gifted':
-            print("Detected: vic_gifted")
-            found_vic_gifted = True
-    
-    if not found_vic and not found_vic_gifted:
-        print("No vic or vic_gifted found.")
-        return False
-    else:
-        return True
+
+        x_min, y_min, x_max, y_max = result.xyxy[0].cpu().numpy().tolist()  # Assuming xyxy format is [x_min, y_min, x_max, y_max]
+
+        # Calculate center
+        pos = [(x_min + x_max) / 2, (y_min + y_max) / 2]
+
+        poses.append(pos)
+
+        print(f"Detected: {class_name}, at pos {pos}")
+
+    return len(poses) > 0
 
