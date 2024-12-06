ca07d5394732d0ddcc341f93eb68d00f6e5c3f92
pirosow <115435792+pirosow@users.noreply.github.com>
2024-12-06 10:53:50 -0500

    Forgot to update paths for planters

--- paths.py
+++ paths.py
@@ -1,6 +1,42 @@
 from functions import *
 import time
 
+def canon(rst=True):
+    loop = 0
+
+    while True:
+        if loop >= 20:
+            break
+
+        if rst or loop > 0:
+            reset()
+
+        time.sleep(0.05)
+
+        press("w", 0.75)
+
+        press("d", 1.1 * (7 - int(readFile("guiFiles/hiveSlot.txt"))))
+
+        keyboard.press("d")
+
+        press(" ", 0.05)
+
+        time.sleep(0.5)
+
+        press("w", 0.2)
+
+        time.sleep(0.5)
+
+        keyboard.release("d")
+
+        time.sleep(0.5)
+
+        if findImg("images/gui/cannon.png", 0.7):
+            break
+
+        loop += 1
+
+
 def PepperVic():
     sendMessage("Searching pepper")
 
@@ -46,7 +82,7 @@ def PepperVic():
     press("s", 0.5)
 
     ShiftLock()
-    
+
     press("d", 0.2)
 
     for _ in range(9):
@@ -64,7 +100,7 @@ def PepperVic():
 
         time.sleep(0.15)
 
-    
+
 def PepperToCannon():
     sendMessage("Moving to cannon")
     press(Key.space, 0.1)
@@ -91,6 +127,7 @@ def PepperToCannon():
     time.sleep(0.87)
     press(Key.space, 0.1)
     time.sleep(1.25)
+
 
 def MountVic():
     sendMessage("Checking mountian")
@@ -119,14 +156,14 @@ def MountVic():
     Waitspeed(2)
     keyboard.release("s")
     time.sleep(0.10)
-    
-    keyboard.tap(Key.page_down) 
+
     keyboard.tap(Key.page_down)
-    keyboard.tap(Key.page_down) 
     keyboard.tap(Key.page_down)
-    keyboard.tap(Key.page_down) 
     keyboard.tap(Key.page_down)
-    keyboard.tap(Key.page_down) 
+    keyboard.tap(Key.page_down)
+    keyboard.tap(Key.page_down)
+    keyboard.tap(Key.page_down)
+    keyboard.tap(Key.page_down)
     keyboard.tap(Key.page_down)
     keyboard.tap(Key.page_down)
     keyboard.tap(Key.page_down)
@@ -145,6 +182,7 @@ def MountVic():
     time.sleep(0.15)
     keyboard.tap("o")
     time.sleep(1)
+
 
 def CactusVic():
     sendMessage("Checking cactus")
@@ -196,6 +234,7 @@ def CactusVic():
     keyboard.tap("o")
     time.sleep(1)
 
+
 def RoseVic():
     sendMessage("Checking rose")
     keyboard.press("w")
@@ -238,6 +277,7 @@ def RoseVic():
     keyboard.tap("i")
     time.sleep(1)
 
+
 def PepperMoveFromDetection():
     press(Key.space, 0.1)
     keyboard.press("d")
@@ -277,13 +317,13 @@ def PepperMoveFromDetection():
     keyboard.release("d")
 
     time.sleep(0.10)
-    
+
 
 def MountMoveFromDetection():
     press(Key.space, 0.1)
     time.sleep(0.1)
     keyboard.press("d")
-    
+
     keyboard.press("d")
     Waitspeed(5.5)
     keyboard.release("d")
@@ -319,6 +359,7 @@ def MountMoveFromDetection():
     keyboard.press("d")
     Waitspeed(11.5)
     keyboard.release("d")
+
 
 def CactusMoveFromDetection():
     keyboard.press("w")
@@ -381,7 +422,7 @@ def RoseMoveFromDetection():
     Waitspeed(10)
     keyboard.release("w")
     time.sleep(0.1)
-    
+
     keyboard.press("d")
     Waitspeed(27)
     keyboard.release("d")
@@ -421,7 +462,8 @@ def RoseMoveFromDetection():
     Waitspeed(6.5)
     keyboard.release("a")
     keyboard.release("w")
-    
+
+
 def PepperKillCycle():
     start_time = time.time()
     timeout = 240  # 4 minutes in seconds
@@ -471,6 +513,7 @@ def PepperKillCycle():
             sendScreenshot("Vic defeated, exiting cycle")
             return True
 
+
 def MountKillCycle():
     start_time = time.time()
     timeout = 240  # 4 minutes in seconds
@@ -519,7 +562,7 @@ def MountKillCycle():
         if detect_vic_defeat():
             sendScreenshot("Vic defeated, exiting cycle")
             return True
-    
+
 
 def CactusKillCycle():
     start_time = time.time()
@@ -530,21 +573,21 @@ def CactusKillCycle():
         if time.time() - start_time > timeout:
             print("4 minute timeout reached")
             return True
-        
+
         # D movement
         for _ in range(6):
-            keyboard.press("w")
+            keyboard.press("d")
             Waitspeed(2)
-            keyboard.release("w")
+            keyboard.release("d")
             if detect_health_in_screenshot():
                 print("Health detected, exiting cycle")
                 return False
 
         # A movement
         for _ in range(7):
-            keyboard.press("s")
+            keyboard.press("a")
             Waitspeed(2)
-            keyboard.release("s")
+            keyboard.release("a")
             if detect_health_in_screenshot():
                 print("Health detected, exiting cycle")
                 return False
@@ -601,6 +644,7 @@ def RoseKillCycle():
         if detect_vic_defeat():
             sendScreenshot("Vic defeated, exiting cycle")
             return True
+
 
 def PepperRespawn():
     press("d", 2)
@@ -698,6 +742,7 @@ def PepperRespawn():
     keyboard.release("w")
     keyboard.release("d")
 
+
 def MountRespawn():
     keyboard.tap("e")
     time.sleep(2)
@@ -708,6 +753,7 @@ def MountRespawn():
     keyboard.press("d")
     Waitspeed(15)
     keyboard.release("d")
+
 
 def CactusRespawn():
     keyboard.tap("e")
@@ -736,6 +782,7 @@ def CactusRespawn():
     keyboard.release("w")
     time.sleep(0.025)
 
+
 def RoseRespawn():
     keyboard.tap("e")
     keyboard.press("s")
@@ -758,13 +805,16 @@ def RoseRespawn():
     keyboard.release("d")
     time.sleep(0.025)
 
+
 import threading
+
 
 def timeout_checker(start_time, timeout=240):
     while True:
         if time.time() - start_time > timeout:
             print("4 minute timeout reached")
             return True
+
         time.sleep(1)
 
 
@@ -773,7 +823,7 @@ def PepperKillCycleLoop():
     timeout_thread = threading.Thread(target=timeout_checker, args=(start_time,))
     timeout_thread.daemon = True
     timeout_thread.start()
-    
+
     while True:
         if not timeout_thread.is_alive():
             return True
@@ -783,12 +833,13 @@ def PepperKillCycleLoop():
         WalkToRedCannon()
         PepperRespawn()
 
+
 def MountKillCycleLoop():
     start_time = time.time()
     timeout_thread = threading.Thread(target=timeout_checker, args=(start_time,))
     timeout_thread.daemon = True
     timeout_thread.start()
-    
+
     while True:
         if not timeout_thread.is_alive():
             return True
@@ -797,12 +848,13 @@ def MountKillCycleLoop():
         Reset_char()
         WalkToRedCannon()
 
+
 def CactusKillCycleLoop():
     start_time = time.time()
     timeout_thread = threading.Thread(target=timeout_checker, args=(start_time,))
     timeout_thread.daemon = True
     timeout_thread.start()
-    
+
     while True:
         if not timeout_thread.is_alive():
             return True
@@ -811,12 +863,13 @@ def CactusKillCycleLoop():
         Reset_char()
         WalkToRedCannon()
 
+
 def RoseKillCycleLoop():
     start_time = time.time()
     timeout_thread = threading.Thread(target=timeout_checker, args=(start_time,))
     timeout_thread.daemon = True
     timeout_thread.start()
-    
+
     while True:
         if not timeout_thread.is_alive():
             return True
@@ -829,108 +882,380 @@ def RoseKillCycleLoop():
 def KillVicBees():
     PepperVic()
 
+    time.sleep(0.1)
+
     img = screenshot()
-    
+
     if detectVicBee(img):
         sendScreenshot("Detected vic bee")
         for _ in range(2):
             time.sleep(0.1)
             keyboard.tap(Key.page_up)
-            
         print("Vic detected, killing bees...")
-        
-        #kill pattern goes here
+        # kill pattern goes here
         PepperMoveFromDetection()
         PepperKillCycleLoop()
         return
-    
+
     for _ in range(2):
         time.sleep(0.1)
         keyboard.tap(Key.page_up)
     PepperToCannon()
-    
     MountVic()
-    
+
+    time.sleep(0.1)
+
     img = screenshot()
-    
+
     if detectVicBee(img):
         sendScreenshot("Detected vic bee")
         for _ in range(2):
             time.sleep(0.1)
             keyboard.tap(Key.page_up)
         print("Vic detected, killing bees...")
-        #kill pattern goes here
+        # kill pattern goes here
         MountMoveFromDetection()
         MountKillCycleLoop()
         return
-    
+
     CactusVic()
 
+    time.sleep(0.1)
+
     img = screenshot()
-    
+
     if detectVicBee(img):
         sendScreenshot("Detected vic bee")
         time.sleep(0.1)
         for _ in range(2):
             time.sleep(0.1)
-            
-            for __ in range(6):
-                keyboard.tap(Key.page_up)
-                
-                time.sleep(0.025)
-
+            keyboard.tap(Key.page_up)
+            keyboard.tap(Key.page_up)
+            keyboard.tap(Key.page_up)
+            keyboard.tap(Key.page_up)
+            keyboard.tap(Key.page_up)
+            keyboard.tap(Key.page_up)
         print("Vic detected, killing bees...")
-        #kill pattern goes here
+        # kill pattern goes here
         CactusMoveFromDetection()
         CactusKillCycleLoop()
         return
-
-    for __ in range(6):
-        keyboard.tap(Key.page_up)
-
-        time.sleep(0.025)
+    keyboard.tap(Key.page_up)
+    keyboard.tap(Key.page_up)
+    keyboard.tap(Key.page_up)
+    keyboard.tap(Key.page_up)
+    keyboard.tap(Key.page_up)
+    keyboard.tap(Key.page_up)
 
     RoseVic()
 
+    time.sleep(0.1)
+
     img = screenshot()
-    
+
     if detectVicBee(img):
         sendScreenshot("Detected vic bee")
         time.sleep(0.1)
-
-        for __ in range(6):
-            keyboard.tap(Key.page_up)
-
-            time.sleep(0.025)
-        
+        keyboard.tap(Key.page_up)
+        keyboard.tap(Key.page_up)
+        keyboard.tap(Key.page_up)
+        keyboard.tap(Key.page_up)
+        keyboard.tap(Key.page_up)
+        keyboard.tap(Key.page_up)
         keyboard.tap(".")
         keyboard.tap(".")
         print("Vic detected, killing bees...")
-        #kill pattern goes here
+        # kill pattern goes here
         RoseMoveFromDetection()
         RoseKillCycleLoop()
         return
 
 
+def canonToPepper(calibrate=False):
+    press("d", 3)
 
-def KillVicFromSearcher(field):
-    if field == "Pepper":
-        PepperVic()
-        PepperMoveFromDetection()
-        PepperKillCycle()
-        return
-    if field == "Mount":
-        MountVic()
-        MountMoveFromDetection()
-        MountKillCycle()
-        return
-    if field == "Cactus":
-        CactusVic()
-        CactusMoveFromDetection()
-        CactusKillCycle()
-        return
-    if field == "Rose":
-        RoseVic()
-        RoseMoveFromDetection()
-        RoseKillCycle()
-        return
+    keyboard.press("d")
+
+    press(" ", 0.05)
+
+    time.sleep(1.4)
+
+    keyboard.release("d")
+
+    time.sleep(0.05)
+
+    keyboard.press("w")
+
+    press(" ", 0.05)
+
+    time.sleep(2.5)
+
+    for _ in range(3):
+        press(" ", 0.05)
+
+        time.sleep(0.7)
+
+    time.sleep(1)
+
+    press(" ", 0.05)
+
+    time.sleep(0.85)
+
+    keyboard.release("w")
+
+    time.sleep(0.05)
+
+    press("w", "d", 2)
+
+    press(" ", 0.05)
+
+    press("d", 2.5)
+
+    press(" ", 0.05)
+
+    press("d", 2.5)
+
+    if calibrate:
+        press("s", 2)
+
+        press("d", 2)
+
+        press("a", 0.5)
+
+        press("w", 1)
+
+
+def canonToMountain(calibrate=False):
+    press("e", 0.05)
+
+    time.sleep(3.5)
+
+
+def canonToPineapple(calibrate=False):
+    canonToMountain()
+
+    press("a", 5.8)
+
+    if calibrate:
+        press("s", 2)
+
+        press("a", 2)
+
+        press("w", 0.5)
+
+        press("w", 0.5)
+
+
+def canonToCactus(calibrate=False):
+    press("e", 0.05)
+
+    time.sleep(0.8)
+
+    for _ in range(2):
+        press(" ", 0.05)
+
+        time.sleep(0.1)
+
+    press("s", "d", 2.5)
+
+    press(" ", 0.05)
+
+    time.sleep(1)
+
+    press("a", 2)
+
+
+def canonToPumpkin(calibrate=False):
+    canonToCactus()
+
+    press("d", 1)
+
+    press("s", 3.5)
+
+    if calibrate:
+        press("s", 2)
+
+        press("a", 2)
+
+        press("w", 0.5)
+
+        press("d", 0.5)
+
+
+def canonToPine(calibrate=False):
+    canonToPumpkin()
+
+    press("d", 5)
+
+    if calibrate:
+        press("s", 2)
+
+        press("d", 2)
+
+        press("a", 0.5)
+
+        press("w", 0.5)
+
+
+def canonToStrawberry(calibrate=False):
+    press("e", 0.05)
+
+    time.sleep(0.75)
+
+    for _ in range(2):
+        press(" ", 0.1)
+
+        time.sleep(0.1)
+
+    press("d", 2)
+
+    press(" ", 0.1)
+
+    time.sleep(1)
+
+    if calibrate:
+        time.sleep(2)
+
+        press("s", 2)
+
+        press("d", 2)
+
+        press("a", 0.5)
+
+        press("w", 0.5)
+
+
+def canonToSpider(calibrate=False):
+    press("e", 0.05)
+
+    time.sleep(0.75)
+
+    for _ in range(3):
+        press(" ", 0.1)
+
+        time.sleep(0.1)
+
+    time.sleep(1)
+
+    press("w", 2)
+
+    if calibrate:
+        time.sleep(2)
+
+        press("s", 2)
+
+        press("d", 2)
+
+        press("a", 0.5)
+
+        press("w", 0.5)
+
+
+def canonToBamboo(calibrate=False):
+    press("e", 0.05)
+
+    time.sleep(0.75)
+
+    for _ in range(2):
+        press(" ", 0.1)
+
+        time.sleep(0.1)
+
+    press("a", 2)
+
+    press(" ", 0.1)
+
+    time.sleep(1)
+
+    press("s", 2)
+
+    if calibrate:
+        time.sleep(2)
+
+        press("s", 2)
+
+        press("a", 2)
+
+        press("w", 0.5)
+
+        press("d", 0.5)
+
+
+def canonToClover(calibrate=False):
+    press("e", 0.05)
+
+    time.sleep(0.25)
+
+    for _ in range(2):
+        press(" ", 0.1)
+
+        time.sleep(0.1)
+
+    press("a", 5)
+
+
+def canonToBlueFlower(calibrate=False):
+    press("e", 0.05)
+
+    time.sleep(0.25)
+
+    for _ in range(2):
+        press(" ", 0.1)
+
+        time.sleep(0.1)
+
+    press("a", 5)
+
+    press("s", 3)
+
+    press("w", 1)
+
+    press("d", 2)
+
+def canonToSunflower(calibrate=False):
+    press("a", 0.5)
+
+    press("s", 5)
+
+    if calibrate:
+        press("d", 2)
+
+        press("w", 3)
+
+        press("s", 1)
+
+        press("a", 0.5)
+
+        press("w", 0.25)
+
+def canonToDandelion(calibrate=False):
+    canonToSunflower()
+
+    press("a", 1.5)
+
+    press(" ", 0.025)
+
+    press("a", 4)
+
+    press("w", 1)
+
+
+def canonToMushroom(calibrate=False):
+    canonToSunflower()
+
+    press("a", 1.5)
+
+    press(" ", 0.025)
+
+    press("a", 3)
+
+    press("s", 2)
+
+    if calibrate:
+        press("s", 2)
+
+        press("a", 4)
+
+        press("d", 0.5)
+
+        press("w", 0.5)
